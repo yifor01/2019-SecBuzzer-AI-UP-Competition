cc24e0d3389fdd2b7d058d057277eb5fc82cf7c7
yifor <howard99664411@gmail.com>
2019-12-19 23:38:46 +0800

    Bug Fix:
    * adjust raw data window size
    * fix Reduce2std_dropoutlier function without outlier (training data confuse)

--- datagenerate.py
+++ datagenerate.py
@@ -15,7 +15,7 @@ logging.basicConfig(level=logging.INFO, format=' %(asctime)s %(message)s')
 df = pd.read_csv('df.csv')
 submit = pd.read_csv('ResultSample.csv')
 logging.info('Parmeters Seting')
-core,start,end,M,DR_mode,reduce_dM,confid = 6,1000,1000000,60000,'mean',1000,0.99
+core,start,end,M,DR_mode,reduce_dM = 14,1000,600000,60000,'mean',1000
 # =============================================================================
 # Original data reduce dimension with mean-std pooling and drop outlier
 # =============================================================================
@@ -29,11 +29,12 @@ train = sparse.csr_matrix(train)
 gc.collect()
 #plt.imshow(train.toarray(), cmap = 'gray',interpolation='nearest', aspect='auto')
 logging.info(f'Get {DR_mode}-std pooling data: reduce {M} to {reduce_dM} dimension')
-new_train = Reduce2std_dropoutlier(train,1000,drop_confid=0.99)
+new_train = Reduce2std(train,reduce_dM)
+
 #plt.imshow(new_train.toarray(), cmap = 'gray',interpolation='nearest', aspect='auto')
 
 logging.info('Save Reduce data')
-file = open(f'train_100w_6w_1k.pickle', 'wb')
+file = open(f'train_{int(end/10000)}w_{int(M/10000)}w_{int(reduce_dM/1000)}k.pickle', 'wb')
 pickle.dump(new_train, file)
 
 # =============================================================================
@@ -45,8 +46,10 @@ with mp.Pool(core) as p:
     test = list(tqdm(p.imap(GetDRdata_md5,submit.md5[:size]), total=size))
 test = sparse.csr_matrix(test)
 logging.info(f'Get {DR_mode}-std pooling data: reduce {M} to {reduce_dM} dimension')
-new_test = Reduce2std_dropoutlier(test,1000,drop_confid=1)
+new_test = Reduce2std(test,reduce_dM)
 
 logging.info('Save Reduce data')
-file = open(f'test_100w_6w_1k.pickle', 'wb')
+file = open(f'test_{int(end/10000)}w_{int(M/10000)}w_{int(reduce_dM/1000)}k.pickle', 'wb')
 pickle.dump(new_test, file)
+
+
